2b2a683f3e641def58223a6530866383116b78e9
Chris Quarshie <chrisquarshie57@gmail.com>
2020-08-03 22:01:24 +0000

    update

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,30 +6,30 @@ const Register = props => {
     return(
         <div>
 
-<div classNameName="container">
-  <div classNameName="row">
-    <div classNameName="col-md-8 col-md-offset-2">
+<div className="container">
+  <div className="row">
+    <div className="col-md-8 col-md-offset-2">
 
-  <p classNameName="body-info">Join KawoLegal. Sign up to get your Startup listed now!</p>
+  <p className="body-info">Join KawoLegal. Sign up to get your Startup listed now!</p>
 
       <form>
-  <div classNameName="form-group">
+  <div className="form-group">
     <label for="exampleInputEmail1">Full name</label>
-    <input type="email" classNameName="form-control" id="exampleInputEmail1" placeholder="Full name"/>
+    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Full name"/>
   </div>
   <div classNameName="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" classNameName="form-control" id="exampleInputEmail1" placeholder="Email"/>
+    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email"/>
   </div>
-  <div classNameName="form-group">
+  <div className="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" classNameName="form-control" id="exampleInputPassword1" placeholder="Password"/>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
   </div>
-  <div classNameName="form-group">
+  <div className="form-group">
     <label for="exampleInputPassword1">Confirm password</label>
-    <input type="password" classNameName="form-control" id="exampleInputPassword1" placeholder="Confirm password"/>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Confirm password"/>
   </div>
- <button type="button" classNameName="btn btn-info">Sign up</button>
+ <button type="button" className="btn btn-info">Sign up</button>
 </form>
     </div>
   </div>
@@ -40,9 +40,9 @@ const Register = props => {
 
 
 
-<div classNameName="main-footer">
-<p classNameName="line-on-footer"></p>
-<footer><p classNameName="text-center">&copy;2017 KawoLegal.All Rights Reserved!</p></footer>
+<div className="main-footer">
+<p className="line-on-footer"></p>
+<footer><p className="text-center">&copy;2017 KawoLegal.All Rights Reserved!</p></footer>
 </div>
 
         </div>
